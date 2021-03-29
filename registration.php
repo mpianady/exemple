78eb9e402ff429079b8d2576b9fb6ea572c9aa7a
Tiana Zo <38865993+mpianady@users.noreply.github.com>
2021-03-29 21:56:40 +0400

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Tiana Zo Alain All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Yara_Akeneo', __DIR__);
+
